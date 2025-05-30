2cffd93a1d11e30c81d1831a6ca1c4a1b3e2ea2c
ramez tayem <rameztayem@gmail.com>
2025-05-30 23:23:20 +0300

    backendUrl

--- constants.ts
+++ constants.ts
@@ -1,3 +1,1 @@
-export const BACKENDURL = "http://192.168.100.5:3000"; // ramez
-// export const BACKENDURL = "http://172.20.10.2:3000"; // ramez mobilehotspot
-//  export const BACKENDURL = "http://localhost:3000"; //mahde
+export const BACKENDURL = "https://takaful-backend-production.up.railway.app";
